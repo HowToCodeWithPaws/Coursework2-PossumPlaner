34d9379c92cd88530f0ce83e337f37ea202fac1c
Zubareva Natalia <ndzubareva@edu.hse.ru>
2021-01-17 00:58:16 +0300

    a screen with a button

---
+++ MyApplication222/app/src/main/java/com/example/myapplication222/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+var set = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.text = "it aint much"
+    }
+
+    fun ChangeText(view: View) {
+        if(!set){
+            set = true
+            button.text = "but it's honest work"
+
+        }else{
+            set = false
+            button.text = "it aint much"
+
+        }
+    }
+}
