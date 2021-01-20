0fed5d598bbc9da890625bf5ce2db06f1c2aa96b
Zubareva Natalia <ndzubareva@edu.hse.ru>
2021-01-20 18:26:54 +0300

    potato-level classes are ready except for user

--- 0.0/app/src/main/java/possumLib/Plan.kt
+++ 0.0/app/src/main/java/possumLib/Plan.kt
@@ -1,6 +1,28 @@
 package possumLib
 
-class Plan(title_: String, isFinished_: Boolean = false, category_:String, importance_: Int = 1) {
+import java.util.*
+
+enum class possibleRepetitions {
+    NONE, DAILY, WEEKLY, MONTHLY
+}
+
+enum class possiblePutOffs {
+    NONE, NEXTDAY, NEXTWEEK, NEXTMONTH
+}
+
+enum class possibleReminders {
+    NONE, H3BEFORE, H12BEFORE, H24BEFORE, H72BEFORE
+}
+
+class Plan(title_: String, isFinished_: Boolean = false,
+           category_: String = "", importance_: Int = 1,
+           subplans_: ArrayList<Subplan> = ArrayList(),
+           repetition_: possibleRepetitions = possibleRepetitions.NONE,
+           putOff_: possiblePutOffs = possiblePutOffs.NONE,
+           reminder_: possibleReminders = possibleReminders.NONE,
+           time_: Double = 0.0,
+           deadline_: Date = java.util.Calendar.getInstance().time,
+           redline_: Date = java.util.Calendar.getInstance().time) {
 
     private var isFinished: Boolean
         get() {
@@ -36,34 +58,91 @@ class Plan(title_: String, isFinished_: Boolean = false, category_:String, impor
             }
         }
 
-    private var importance : Int
-    get(){return importance}
-    set(value){if(value in 0..4){importance = value}}
+    private var importance: Int
+        get() {
+            return importance
+        }
+        set(value) {
+            if (value in 0..4) {
+                importance = value
+            }
+        }
+
+    private var repetition: possibleRepetitions
+        get() {
+            return repetition
+        }
+        set(value) {
+            repetition = value
+        }
+
+    private var subplans: ArrayList<Subplan>
+        get() {
+            return subplans
+        }
+        set(value) {
+            subplans = value
+        }
+
+    fun addSubplan(subplan: Subplan) {
+        subplans.add(subplan)
+    }
+
+    fun deleteSubplan(subplan: Subplan) {
+        subplans.remove(subplan)
+    }
+
+    private var putOff: possiblePutOffs
+        get() {
+            return putOff
+        }
+        set(value) {
+            putOff = value
+        }
+
+    private var reminder: possibleReminders
+        get() {
+            return reminder
+        }
+        set(value) {
+            reminder = value
+        }
+
+    private var time: Double
+        get() {
+            return time
+        }
+        set(value) {
+            if (value >= 0) time = value
+        }
+
+    private var deadline: Date
+        get() {
+            return deadline
+        }
+        set(value) {
+            deadline = value
+        }
+
+    private var redline: Date
+        get() {
+            return redline
+        }
+        set(value) {
+            redline = value
+        }
 
     init {
         isFinished = isFinished_
         title = title_
         importance = importance_
         category = category_
+        repetition = repetition_
+        subplans = subplans_
+        putOff = putOff_
+        time = time_
+        deadline = deadline_
+        redline = redline_
     }
 }
-//дата, время срока выполнения;
-//предполагаемое время, которое нужно для выполнения;
-//подзадачи со следующими параметрами
-//повторяемость дедлайна, с выбором из следующих значений
-//не повторять;
-//ежедневно;
-//еженедельно;
-//ежемесячно;
-//произвольно;
-//перенос дедлайна по истечении срока выполнения с выбором следующих опций:
-//увеличить срок выполнения;
-//отметить дедлайн как выполненный;
-//ничего не совершать;
-//напоминания со следующими опциями:
-//не напоминать;
-//напомнить за 3 часа до срока выполнения;
-//напомнить за 12 часов до срока выполнения;
-//напомнить за 1 день до срока исполнения;
-//напомнить за 3 дня до срока выполнения;
-//пользовательский выбор времени напоминания;
+
---
+++ 0.0/app/src/main/java/possumLib/User.kt
@@ -0,0 +1,4 @@
+package possumLib
+
+class User {
+}
